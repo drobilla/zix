7f9f0d2b6e7ac33e5b2cc5060481a2cc1d429a7d
David Robillard <d@drobilla.net>
2019-10-18 20:07:17 +0200

    Fix zix_sem_init return value on Windows

--- zix/sem.h
+++ zix/sem.h
@@ -150,7 +150,7 @@ static inline ZixStatus
 zix_sem_init(ZixSem* sem, unsigned initial)
 {
 	sem->sem = CreateSemaphore(NULL, initial, LONG_MAX, NULL);
-	return (sem->sem) ? ZIX_STATUS_ERROR : ZIX_STATUS_SUCCESS;
+	return (sem->sem) ? ZIX_STATUS_SUCCESS : ZIX_STATUS_ERROR;
 }
 
 static inline void
