5891e6fb17de98fae6764bdff929bcaa21a1b36c
David Robillard <d@drobilla.net>
2022-10-23 13:41:40 -0400

    Add C++ header build test

---
+++ test/cpp/test_headers_cpp.cpp
@@ -0,0 +1,29 @@
+// Copyright 2022 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#include "zix/allocator.h"      // IWYU pragma: keep
+#include "zix/attributes.h"     // IWYU pragma: keep
+#include "zix/bitset.h"         // IWYU pragma: keep
+#include "zix/btree.h"          // IWYU pragma: keep
+#include "zix/bump_allocator.h" // IWYU pragma: keep
+#include "zix/digest.h"         // IWYU pragma: keep
+#include "zix/filesystem.h"     // IWYU pragma: keep
+#include "zix/function_types.h" // IWYU pragma: keep
+#include "zix/hash.h"           // IWYU pragma: keep
+#include "zix/path.h"           // IWYU pragma: keep
+#include "zix/ring.h"           // IWYU pragma: keep
+#include "zix/sem.h"            // IWYU pragma: keep
+#include "zix/status.h"         // IWYU pragma: keep
+#include "zix/string_view.h"    // IWYU pragma: keep
+#include "zix/thread.h"         // IWYU pragma: keep
+#include "zix/tree.h"           // IWYU pragma: keep
+#include "zix/zix.h"            // IWYU pragma: keep
+
+#if defined(__GNUC__)
+__attribute__((const))
+#endif
+int
+main()
+{
+  return 0;
+}
